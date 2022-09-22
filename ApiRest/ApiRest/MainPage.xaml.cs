376eb4f8fdd503e39528d618d3d9385c8e62ae3b
romariou76 <illeniumr@gmail.com>
2022-09-22 11:57:42 -0500

    Primer proyecto consumo de Api Rest

---
+++ ApiRest/ApiRest/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ApiRest.Modelos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ApiRest
+{
+    public partial class MainPage : ContentPage
+    {
+        private string url = "https://jsonplaceholder.typicode.com/todos";
+        HttpClient cliente = new HttpClient();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            string contenido = await cliente.GetStringAsync(url);
+            IEnumerable<UsuarioModel> lista = JsonConvert.DeserializeObject<IEnumerable<UsuarioModel>>(contenido);
+            ltusuario.ItemsSource=new ObservableCollection<UsuarioModel>(lista);
+            base.OnAppearing();
+        }
+    }
+}
---
+++ ApiRest/ApiRest/Modelos/UsuarioModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ApiRest.Modelos
+{
+    public class UsuarioModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChange([CallerMemberName] string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+        private int userid;
+
+        public int UserId
+        {
+            get { return userid; }
+            set
+            {
+                userid = value;
+                OnPropertyChange();
+            }
+        }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChange(); }
+        }
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; OnPropertyChange(); }
+        }
+        private bool complete;
+
+
+
+        public bool Complete
+        {
+            get { return complete; }
+            set { complete = value; OnPropertyChange(); }
+        }
+
+
+
+    }
+}
